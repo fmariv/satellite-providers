4a2f7a47b0b4a1029573bec14a1d652d847b0a8a
Fran Martin <fran@earthpulse.ai>
2023-05-16 09:56:27 +0200

    Add Streamlit and remove bands

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+def main():
+    st. set_page_config(layout="wide")
+    # Read README.md file
+    with open('readme.md', 'r', encoding='utf-8') as file:
+        readme_content = file.read()
+
+    # Show README.md file as a web page
+    st.markdown(readme_content, unsafe_allow_html=True)
+
+
+if __name__ == '__main__':
+    main()
